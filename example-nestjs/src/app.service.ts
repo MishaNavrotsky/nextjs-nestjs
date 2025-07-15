9af9e98ad4016fe5774a211f44a10760e1802982
Misha <mi.navrotskyy@gmail.com>
2025-07-15 17:39:35 +0300

    Added docker-compose

--- example-nestjs/src/app.service.ts
+++ example-nestjs/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World112342321231fw32234ef242!';
   }
 }
