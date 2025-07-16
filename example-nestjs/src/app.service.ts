d484f7f843ee3828ed063c05d6456794a08d8bd2
Misha <mi.navrotskyy@gmail.com>
2025-07-16 19:33:54 +0300

    Added Redis and local env for dev

--- example-nestjs/src/app.service.ts
+++ example-nestjs/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World112342321231fw32234ef242!';
+    return 'Hello World!';
   }
 }
